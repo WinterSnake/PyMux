10de4bde3530b94ab153185100017aa9994d5f82
Winter_Snake <blizzardsnake98@gmail.com>
2025-08-06 18:19:51 -0400

    refactor: widget parser
    fix: checksum calculator

--- pymux/widget.py
+++ pymux/widget.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
 ##-------------------------------##
-## PyMux                         ##
+## PyMux Session                 ##
 ## Written By: Ryan Smith        ##
 ##-------------------------------##
 ## Widget                        ##
@@ -8,10 +7,11 @@
 
 ## Imports
 from __future__ import annotations
-from typing import ClassVar, Literal, Self
+from collections.abc import Iterable
+from typing import Literal
 
 ## Constants
-type ORIENTATION = Literal['horizontal', 'vertical'] | None
+ORIENTATION = Literal["horizontal"] | Literal["vertical"] | None
 
 
 ## Functions
@@ -20,162 +20,138 @@ def calculate_checksum(source: str) -> int:
     checksum: int = 0
     for c in source:
         checksum = (checksum >> 1) + ((checksum & 1) << 15)
-        checksum += ord(c)
+        checksum = (checksum + ord(c)) & 0xFFFF
     return checksum
 
-
-def _parse_widget(source: str, offset: int) -> tuple[int, Widget]:
-    """Parse tmux widget structure with id or nesting"""
-    offset, obj = _parse_dimensions(source, offset)
-    # -Object Id
-    if source[offset] == ',':
-        offset, obj._id = _parse_int(source, offset + 1)
-    # -Object Nesting: Horizontal
-    elif source[offset] == '{':
-        obj.orientation = 'horizontal'
-        while source[offset] != '}':
-            offset, child = _parse_widget(source, offset + 1)
-            obj.children.append(child)
-        offset += 1
-    # -Object Nesting: Vertical
-    elif source[offset] == '[':
-        obj.orientation = 'vertical'
-        while source[offset] != ']':
-            offset, child = _parse_widget(source, offset + 1)
-            obj.children.append(child)
-        offset += 1
-    return (offset, obj)
+def parse_widget(source: str) -> tuple[str, Widget]:
+    """Parse a tmux source string into a nested widget"""
+    source, dimensions = _parse_dimensions(source)
+    # -Id
+    if source[0] == ',':
+        source, _id = _parse_int(source[1:])
+        return (source, Widget(dimensions, _id))
+    # -Nest: Horizontal
+    elif source[0] == '{':
+        source, children = _parse_children(source[1:])
+        widget = Widget(dimensions, None, 'horizontal', children)
+        assert source[0] == '}'
+        return (source[1:], widget)
+    # -Nest: Vertical
+    elif source[0] == '[':
+        source, children = _parse_children(source[1:])
+        widget = Widget(dimensions, None, 'vertical', children)
+        assert source[0] == ']'
+        return (source[1:], widget)
+    raise ValueError(f"Invalid layout string: {source}")
 
 
-def _parse_dimensions(source: str, offset: int) -> tuple[int, Widget]:
-    """Parse tmux widget dimensions (x, y, w, h)"""
-    offset, width = _parse_int(source, offset)
-    offset, height = _parse_int(source, offset + 1)
-    offset, x = _parse_int(source, offset + 1)
-    offset, y = _parse_int(source, offset + 1)
-    widget = Widget(x, y, width, height)
-    return (offset, widget)
+def _parse_children(source: str) -> tuple[str, list[Widget]]:
+    """Return all parsed children widgets"""
+    children = []
+    source, widget = parse_widget(source)
+    children.append(widget)
+    while source[0] == ',':
+        source, widget = parse_widget(source[1:])
+        children.append(widget)
+    return (source, children)
 
 
-def _parse_int(source: str, offset: int) -> tuple[int, int]:
-    """Parse int from source"""
-    value: int = 0
-    while len(source) > offset and source[offset].isdigit():
-        value = value * 10 + int(source[offset])
-        offset += 1
-    return (offset, value)
+def _parse_dimensions(source: str) -> tuple[str, tuple[int, int, int, int]]:
+    """Parse widget dimensions from tmux source string {x, y, w, h}"""
+    source, width = _parse_int(source)
+    source, height = _parse_int(source[1:])
+    source, x = _parse_int(source[1:])
+    source, y = _parse_int(source[1:])
+    return (source, (x, y, width, height))
+
+
+def _parse_int(source: str) -> tuple[str, int]:
+    """Returns parsed integer"""
+    value: str = ''
+    while len(source) > 0 and source[0].isdigit():
+        value += source[0]
+        source = source[1:]
+    return (source, int(value))
 
 
 ## Classes
 class Widget:
     """
-    Tmux Window/Pane
-    Represents a window or a pane in a singular class
+    Tmux Widget 
+    Represents a tmux window or a pane in a singular class
+    Uses orientation nesting to accomplish pane divisions inside a window widget
     """
 
     # -Constructor
-    def __init__(self, x: int, y: int, width: int, height: int) -> None:
-        self._id: int | None = None
-        # -Dimensions
-        self.x: int = x
-        self.y: int = y
-        self.width: int = width
-        self.height: int = height
-        # -Children
-        self.orientation: ORIENTATION = None
-        self.children: list[Widget] = []
+    def __init__(
+        self, dimensions: tuple[int, int, int, int],
+        _id: int | None, orientation: ORIENTATION = None,
+        children: Iterable[Widget] | None = None
+    ) -> None:
+        self.id: int | None = _id
+        self.position: tuple[int, int] = (dimensions[0], dimensions[1])
+        self.size: tuple[int, int] = (dimensions[2], dimensions[3])
+        self.orientation: ORIENTATION = orientation
+        self.children: Iterable[Widget] | None = children
 
     # -Dunder Methods
-    def __iter__(self) -> Self:
-        self._index: int = 0
-        self._index_stack: list[Widget] = self.children[:]
-        return self
-
-    def __next__(self) -> Widget:
-        if self._index >= len(self):
-            raise StopIteration()
-        widget: Widget
-        if not self.children:
-            widget = self
-        else:
-            while self._index_stack:
-                widget = self._index_stack.pop()
-                if not widget.children:
-                    break
-                self._index_stack.extend(widget.children)
-        self._index += 1
-        return widget
-
-    def __len__(self) -> int:
-        if not self.children:
-            return 1
-        count: int = 0
-        child_stack: list[Widget] = self.children[:]
-        while child_stack:
-            child = child_stack.pop()
-            if child.children:
-                child_stack.extend(child.children)
-            else:
-                count += 1
-        return count
-
     def __str__(self) -> str:
-        widget: str = f"{self.width}x{self.height},{self.x},{self.y}"
+        _str = f"{self.width}x{self.height},{self.x},{self.y}"
+        # -Id
         if not self.children:
-            return widget + f",{self.id}"
-        children: str = ','.join(str(child) for child in self.children)
+            return _str + f",{self.id}"
+        # -Children
+        children = ','.join(str(child) for child in self.children)
         if self.orientation == 'horizontal':
-            return widget + '{' + children + '}'
-        elif self.orientation == 'vertical':
-            return widget + '[' + children + ']'
-        raise ValueError("Orientation expected to be 'horizontal' or 'vertical', got: '{self.orientation}'")
-
-    # -Instance Methods
-    def find_by_id(self, _id: int) -> Widget | None:
-        '''Find a given widget by it's id'''
-        if self._id == _id:
-            return self
-        for child in self.children:
-            if (w := child.find_by_id(_id)) is not None:
-                return w
-        return None
-
-    def sort(self) -> None:
-        '''Sort widget and it's children by X/Y'''
-        if not self.children:
-            return
-        self.children.sort(key=lambda k: (k.x, k.y))
-        for child in self.children:
-            child.sort()
+            return _str + '{' + children + '}'
+        else:
+            return _str + '[' + children + ']'
 
     # -Class Methods
     @classmethod
-    def from_layout(cls, source: str) -> Widget:
-        '''Create a widget from a given tmux layout string with checksum validation'''
-        checksum = int(source[:4], 16)
-        source = source[5:]
-        assert checksum == calculate_checksum(source)
-        return _parse_widget(source, 0)[1]
+    def from_layout(cls, layout: str) -> Widget:
+        '''Validate widget from a source and return widget'''
+        checksum = int(layout[:4], 16)
+        source = layout[5:]
+        calculated_checksum = calculate_checksum(source)
+        if calculated_checksum != checksum:
+            raise ValueError(f"Checksum failed, expected: {checksum:04x}; actual: {calculated_checksum:04x}")
+        assert calculate_checksum(source) == checksum
+        return parse_widget(source)[1]
 
     # -Properties
     @property
-    def id(self) -> int:
-        if self._id is None:
-            self._id = Widget._Id
-            Widget._Id += 1
-        return self._id
-
-    @property
     def layout(self) -> str:
-        return f"{calculate_checksum(str(self)):04x},{self}"
+        return f"{calculate_checksum(str(self)):04x},{str(self)}"
 
     @property
-    def position(self) -> tuple[int, int]:
-        return (self.x, self.y)
+    def x(self) -> int:
+        return self.position[0]
+
+    @x.setter
+    def x(self, value: int) -> None:
+        self.position = (value, self.y)
 
     @property
-    def size(self) -> tuple[int, int]:
-        return (self.width, self.height)
+    def y(self) -> int:
+        return self.position[1]
 
-    # -Class Properties
-    _Id: ClassVar[int] = 1
+    @y.setter
+    def y(self, value: int) -> None:
+        self.position = (self.x, value)
+
+    @property
+    def width(self) -> int:
+        return self.size[0]
+
+    @width.setter
+    def width(self, value: int) -> None:
+        self.size = (value, self.height)
+
+    @property
+    def height(self) -> int:
+        return self.size[1]
+
+    @height.setter
+    def height(self, value: int) -> None:
+        self.size = (self.width, value)
